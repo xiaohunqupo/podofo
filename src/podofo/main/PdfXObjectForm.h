f5674d93e23bf0062fcb0011562ea0f89cc2407b
Francesco Pretto <ceztko@gmail.com>
2026-04-28 10:18:36 +0200

    PdfXObjectForm: Improved comments for FillFromPage method

--- src/podofo/main/PdfXObjectForm.h
+++ src/podofo/main/PdfXObjectForm.h
@@ -72,7 +72,6 @@ private:
     PdfObjectStream& ResetContentsStream() override;
     void CopyContentsTo(OutputStream& stream) const override;
     void initXObject(const Rect& rect);
-    void initAfterPageInsertion(const PdfPage& page);
 
 private:
     // Remove some PdfCanvas methods to maintain the class API surface clean
